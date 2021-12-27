f177eba6b94f539f0288929a38b2b8fef5e423fd
Maciej Kleban <dawnwender@gmail.com>
2021-12-27 20:57:44 +0100

    Linter (#4)
    * Enforce pylint approved code on every pull request
    * Update README.md
    * Add comments to all files and docstrings

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ This repository contains the following files:
 2. `logging.py` - setup for logging:
 * Logs of level `DEBUG` will be saved to a file named `plotter.log`
 * Logs of level `INFO` will be printed to the console
+3. `database.py` - script storing images and retrieving them from the database
+
+## Contributing
+Before submitting your pull request please check your code with pylint - settings for pylint are available in `.pylintrc` file. Github Actions will automatically run it on your code on every pull request. Please also remember this is a school project therefore the repository may become inactive after a while when its authors will graduate.
 
 ## Copyrights
 This project is licensed under **MIT** license, which means that you have the right to distribute, modify and use this project in your own projects (even under more strict license) so long as you provide the original authors whose work you are using in your project and do not hold them liable.
--- database.py
+++ database.py
@@ -6,33 +6,64 @@ logger = logging.getLogger()
 
 
 class Database:
+    """
+    Node resposible for storing images in the database and retrieving them from it.
+    :param con: Connection object used to connect to database with
+    :param cur: Cursor obeject used to invoke commands on the database
+    """
     def __init__(self, con = None, cur = None):
         self.con = con
         self.cur = cur
 
 
     def setup(self, dbpath):
+        '''
+        Connect to the database and create a cursor.
+        :param dbpath: Absolute path to the database
+        :type dbpath: str
+        '''
         self.con = sqlite3.connect(dbpath)
         self.cur = self.con.cursor()
-    
+
 
     def create_table(self, table_name):
-        logger.debug("Creating new table {0}...", table_name)
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS %s 
+        '''
+        Create new table to store data in.
+        Default data types in table are: (id, date, name, image).
+        :param table_name: Table name to create
+        :type table_name: str
+        '''
+        logger.debug("Creating new table %s...", table_name)
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS %s
         (id NUMBER PRIMARY KEY, date DATE, name VARCHAR(30), image BLOB)''' % table_name)
-        logger.info("Table {0} created!", table_name)
+        logger.info("Table %s created!", table_name)
 
 
     def store_image(self, filepath, table):
-        logger.debug("Storing image {0} into table {1}...".format(filepath, table))
-        with open(filepath, 'rb') as f:
+        '''
+        Store image inside of the database.
+        :param filepath: Absolute path to the image file
+        :type filepath: str
+        :param table: Name of the table to store results in
+        :type table: str
+        '''
+        logger.debug("Storing image %s into table %s...", filepath, table)
+        with open(filepath, 'rb') as file:
             self.con.execute('''INSERT INTO {0} (date, name, image)
-            VALUES({1}, {2}, {3})'''.format(table, datetime.now(), filepath, sqlite3.Binary(f.read())))
+            VALUES({1}, {2}, {3})'''.format(table, datetime.now(),
+                                            filepath, sqlite3.Binary(file.read())))
             self.con.commit()
-        logger.info("Image {0} saved!".format(filepath))
-    
+        logger.info("Image %s saved!", filepath)
+
 
     def retrieve_image(self, table, filepath):
-        logger.debug("Retrieving image {0} from table {1}...".format(filepath, table))
+        '''
+        Fetch image from the database.
+        :param table: Table name to fetch image from
+        :type table: str
+        :param filepath: Absolute filepath of the image to fetch
+        :type filepath: str
+        '''
+        logger.debug("Retrieving image %s from table %s...", filepath, table)
         self.cur.execute('''SELECT image FROM {0} WHERE name = {1}'''.format(table, filepath))
         return self.cur.fetchone()
