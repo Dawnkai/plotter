8930ef9c00b54a9ea503fb9310a103ae613c2864
Maciej Kleban <dawnwender@gmail.com>
2022-02-01 00:46:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import time
 from flask import Flask, render_template, jsonify, request
 from database import Database
+#from camera import Camera
 from logger import setup_logger
 import logging
 
 app = Flask(__name__)
 db = Database("data.db")
+#cam = Camera()
 logger = logging.getLogger()
 
 def setup():
@@ -13,25 +16,43 @@ def setup():
     db.init_state("Status")
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return render_template("home.html")
 
 
 @app.route("/plot", methods=["GET", "POST"])
 def plot_page():
+    # GET
     if request.method == "GET":
         return render_template("plot.html")
-    elif request.method == "POST":
-        plotting = db.get_state("Status")
-        if plotting == "Idle":
-            db.change_state("Status", "Busy")
-            # TODO : Plot
-            db.change_state("Status", "Idle")
-            return jsonify(result="Plotting finished.")
-        else:
-            resp = jsonify({'message': 'Plotter is busy.'})
-            return resp, 403
+    
+    # POST
+    plotting = db.get_state("Status")
+    if plotting == "Idle":
+        db.change_state("Status", "Busy")
+        # TODO : Plot
+        db.change_state("Status", "Idle")
+        return jsonify(result="Plotting finished.")
+    else:
+        resp = jsonify({'message': 'Plotter is busy.'})
+        return resp, 403
+
+
+@app.route("/camera", methods=["GET", "POST"])
+def take_picture():
+    # GET
+    if request.method == "GET":
+        return render_template("camera.html")
+
+    # POST
+    else:
+        try:
+            #cam.take_picture()
+            return jsonify({'message': 'Picture taken successfully.'}), 201
+        except Exception as e:
+            logger.error("Error while taking a picture: %s", e)
+            return jsonify({'message': 'Problem with camera'}), 403
 
 
 if __name__ == "__main__":
